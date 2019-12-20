b1c8e8bef283d2ad667b3e91f9cd01229a0bcc79
gowild <787821043@qq.com>
2019-12-20 11:50:32 +0800

    修改管理员修改问题1

---
+++ bbs/src/com/bbs/dao/impl/GradeDaoImpl.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.bbs.dao.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.bbs.bean.Grades;
+import com.bbs.dao.GradeDao;
+
+
+public class GradeDaoImpl implements GradeDao{
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	@Override
+	public Grades find(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		Grades grade = (Grades) session.get(Grades.class, id);
+		return grade;
+	}
+
+}
---
+++ bbs/src/com/bbs/dao/impl/HelpDaoImpl.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.bbs.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bbs.bean.Helps;
+import com.bbs.dao.HelpDao;
+
+
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT)
+public class HelpDaoImpl implements HelpDao {
+
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public List<Helps> getAll() {
+		Session session = sessionFactory.getCurrentSession();
+		String hql="from Helps hel order by hel.id asc";
+		List<Helps> listHelp = null;
+		try {
+			Query q = session.createQuery(hql);
+			listHelp = q.list();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		return listHelp;
+	}
+
+	@Override
+	public Helps find(int index) {
+		Session session = sessionFactory.getCurrentSession();
+		Helps thelp = (Helps) session.get(Helps.class, index);
+		return thelp;
+	}
+
+	@Override
+	public void add(Helps thelp) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(thelp);
+	}
+	@Override
+	public void update(Helps thelp) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(thelp);
+	}
+
+}
